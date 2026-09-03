9fc0474eaa5c1e3c39795580a370c98a32d48429
James Taylor <jta164@uclive.ac.nz>
2026-09-04 00:44:50 +1200

    style: apply clang-format

--- src/bus.cpp
+++ src/bus.cpp
@@ -1,11 +1,5 @@
 #include "bus.hpp"
 
-void Bus::write(uint16_t addr, uint8_t data)
-{
-    memory[addr] = data;
-}
+void Bus::write(uint16_t addr, uint8_t data) { memory[addr] = data; }
 
-uint8_t Bus::read(uint16_t addr) const
-{
-    return memory[addr];
-}
+uint8_t Bus::read(uint16_t addr) const { return memory[addr]; }
