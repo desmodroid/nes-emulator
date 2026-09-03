3c8c9b1cb97de8934a4feb3ef1939bc9d40badc0
James Taylor <jta164@uclive.ac.nz>
2026-09-04 00:56:04 +1200

    docs: added doc comments to status flags

--- include/cpu.hpp
+++ include/cpu.hpp
@@ -1,15 +1,16 @@
 #pragma once
 
+#include <cstdint>
 class Cpu {
 public:
-  enum StatusFlag {
-    Carry = 1 << 0,
-    Zero = 1 << 1,
-    Interrupt = 1 << 2,
-    Decimal = 1 << 3,
-    B = 1 << 4,
-    Unused = 1 << 5,
-    Overflow = 1 << 6,
-    Negative = 1 << 7,
+  enum StatusFlag : uint8_t {
+    Carry = 1 << 0,     // ADC produces a carry
+    Zero = 1 << 1,      // Set when value is equal to zero
+    Interrupt = 1 << 2, // Set when you want to disable interrupts
+    Decimal = 1 << 3,   // On NES this is disabled so no touchy
+    Break = 1 << 4,     // Set when pushed by BRK or PHP
+    Unused = 1 << 5,    // Pushed as 1
+    Overflow = 1 << 6,  // Set when signed result is invalid (overflow)
+    Negative = 1 << 7,  // Set when the result 7th bit is 1 (negative)
   };
 };
