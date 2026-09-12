5c811e75786ea3cbd6af90d6db0149f7f6f427bc
James Taylor <jta164@uclive.ac.nz>
2026-09-12 14:14:44 +1200

    fix: fixed sbc implementation, store inverted memory value

--- src/cpu_instructions.cpp
+++ src/cpu_instructions.cpp
@@ -69,11 +69,11 @@ void Cpu::adc(uint16_t addr) {
 
 void Cpu::sbc(uint16_t addr) {
   uint8_t memoryValue = bus.read(addr);
-  memoryValue = ~memoryValue;
+  uint8_t invertedMemoryValue = ~memoryValue;
   uint8_t carryBit = getFlag(Carry) ? 1 : 0;
-  uint16_t sum = a + memoryValue + carryBit;
+  uint16_t sum = a + invertedMemoryValue + carryBit;
   uint8_t result = static_cast<uint8_t>(sum);
-  bool overflow = ((a ^ result) & (result ^ memoryValue) & 0x80) != 0;
+  bool overflow = ((a ^ result) & (result ^ invertedMemoryValue) & 0x80) != 0;
   a = result;
 
   updateZeroNegativeFlags(a);
